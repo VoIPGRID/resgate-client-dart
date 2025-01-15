18e2863c478fc3e10e2e3b4787454fc9aede0f4f
Patrick <patrick.swijgman@wearespindle.com>
2025-01-15 15:28:40 +0100

    chore: anonymized example

--- example/example.dart
+++ example/example.dart
@@ -1,13 +1,13 @@
 import 'package:resgate_client/client.dart';
 import 'package:resgate_client/model.dart';
 
-class QueueModel extends ResModel {
-  int waitingCallers = 0;
+class MyModel extends ResModel {
+  int counter = 0;
 
   @override
   void updateFromJson(Map<String, dynamic> json) {
-    if (json.containsKey("waiting_callers")) {
-      waitingCallers = json["waiting_callers"];
+    if (json.containsKey("counter")) {
+      counter = json["counter"];
     }
   }
 }
@@ -15,35 +15,35 @@ class QueueModel extends ResModel {
 void main() async {
   var client = ResClient();
 
-  client.connect('wss://frontend-resgate-mocking.eu-staging.holodeck.voys.nl');
+  client.connect('wss://example.com');
 
   // You need to authenticate in order to subscribe.
   // Try commenting this out to see an access denied error.
   await client.authenticate(
     "usertoken.login",
     {
-      "token": "my-vg-api-token",
+      "token": "my-api-token",
     },
   );
 
   // Subscribe to a subject, supply the model factory so that each model
   // can be updated on a change event. Hence this model should extend ResModel
   // and implement the `updateFromJson` method.
-  var collection = await client.getCollection(
-      "dashboard.client.abc-123", () => QueueModel());
+  var collection =
+      await client.getCollection("example.collection.1", () => MyModel());
 
   // React when a model is added or removed from a collection.
   collection.onAdd(print);
   collection.onRemove(print);
 
   // A collection holds an internal list of models that can be read.
-  var model = collection.models[2];
+  var model = collection.models[0];
 
   // React when a model updates.
   model.onChange((values) {
     print(values);
     // You can also get the updated value straight from the model instance.
-    print(model.waitingCallers);
+    print(model.counter);
   });
 
   // When you're done with the collection you can call destroy on it.
